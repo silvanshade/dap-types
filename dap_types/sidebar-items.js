dada310f6fd7d6366a6bae6b4463e3b1442095b1
silvanshade <silvanshade@users.noreply.github.com>
2021-04-01 23:18:21 +0000

    deploy: 3ec0f17521bf71fb9a0ae78647db2d24e1ff0454

---
+++ dap_types/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["events",""],["protocol",""],["requests",""],["types",""]]});
---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"dap_types":{"doc":"","t":[0,0,0,0,0,0],"n":["events","protocol","requests","client","server","types"],"q":["dap_types","","","dap_types::requests","","dap_types"],"d":["","","","","",""],"i":[0,0,0,0,0,0],"f":[null,null,null,null,null,null],"p":[]}\
+}');
+initSearch(searchIndex);
