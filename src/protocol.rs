c0289c814252280dc3563d9dd1022fad78fc3058
silvanshade <silvanshade@users.noreply.github.com>
2021-04-01 17:41:33 -0600

    Remove protocol_message

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,11 +1,9 @@
 mod error_response;
 mod event;
-mod protocol_message;
 mod request;
 mod response;
 
 pub use error_response::*;
 pub use event::*;
-pub use protocol_message::*;
 pub use request::*;
 pub use response::*;
