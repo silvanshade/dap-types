14e7e96df6ae77e2c3c3cfd671e9152ab4694753
silvanshade <silvanshade@users.noreply.github.com>
2021-04-04 11:25:11 -0600

    Define Request trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
+use serde::{de, Serialize};
+
 pub mod events;
-pub mod protocol;
 pub mod requests;
 pub mod types;
+
+pub trait Request {
+    type Arguments: de::DeserializeOwned + Serialize;
+    type Result: de::DeserializeOwned + Serialize;
+    const COMMAND: &'static str;
+}
