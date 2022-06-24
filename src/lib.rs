cc404530d9e4c5c9e687450d3dffbcc5e273ae6b
silvanshade <silvanshade@users.noreply.github.com>
2022-06-23 21:11:53 -0600

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,8 @@ pub mod events;
 pub mod requests;
 pub mod types;
 
-pub trait Request {
+pub trait RequestResponse {
     type Arguments: de::DeserializeOwned + Serialize;
-    type Result: de::DeserializeOwned + Serialize;
+    type Body: de::DeserializeOwned + Serialize;
     const COMMAND: &'static str;
 }
--- src/types/exception_path_segment.rs
+++ src/types/exception_path_segment.rs
@@ -1,1 +1,4 @@
+use serde::{Deserialize, Serialize};
 
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct ExceptionPathSegment;
